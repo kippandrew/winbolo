53956b511a304ba3be0e3fda6555985975b023d5
jhoodshow <jhoodshow@f5f8f8c0-f940-0410-bb6b-693536db5c03>
2009-07-13 03:29:33 +0000

    Finalized version of server-kick functionality. (removed debugging code)
    git-svn-id: http://winbolo.googlecode.com/svn/trunk@165 f5f8f8c0-f940-0410-bb6b-693536db5c03

--- winbolo/src/server/servermain.c
+++ winbolo/src/server/servermain.c
@@ -160,10 +160,10 @@ void processKeys(bool isQuiet) {
 	char keyBuff[256] = "\0";
 	char saveBuff[256] = "\0";
 
-	char kickMsg[256] = "\0";
-	char name[255] = "\0";
-	char playerKick[255] = "\0";
-	char tempBuffer[255] = "\0";
+	char kickMsg[128] = "\0";
+	char name[32] = "\0";
+	char playerKick[33] = "\0";
+	char tempBuffer[128] = "\0";
 
 	int i=0;
 	size_t newbuflen;
@@ -190,29 +190,15 @@ void processKeys(bool isQuiet) {
 			} else if (strncmp(keyBuff, "say ", 4) == 0) {
 				serverNetSendServerMessageAllPlayers((char *) keyBuff+4);
 			} else if (strncmp(keyBuff, "kick ", 5) == 0) {
-
-/*				strcpy(playerKick, (char *) keyBuff+5);	*/
-				sprintf(playerKick, "%.*s", 20, keyBuff+5);
-
+				sprintf(playerKick, "%.*s", 32, keyBuff+5);
 				newbuflen = strlen(playerKick);
 				playerKick[newbuflen - 1] = '\0';
-
-/*wsprintf(tempBuffer, "name: [%s], len: %d\n\n", playerKick, strlen(playerKick)); */
-fprintf(stderr, tempBuffer);
-
 				for(i=0;i<=15;i++){
 					playersGetPlayerName(screenGetPlayers(), i, name);
-
-/*wsprintf(tempBuffer, "name: [%s], len: %d\n", name, strlen(name)); */
-fprintf(stderr, tempBuffer);
-
-
 					if(strcmp(playerKick, name) == 0){
-
 						sprintf(kickMsg, "%s has been server kicked.", playerKick);
 						serverNetSendServerMessageAllPlayers(kickMsg);
 						serverNetPlayerLeave(i, TRUE);
-
 					}
 				}
 			} else if (strncmp(keyBuff, "\n", 1) != 0 && strncmp(keyBuff, "\0", 1) != 0) {
@@ -236,10 +222,10 @@ void processKeys() {
   struct timeval timer; /*select timer */
   int ret;
 
-  	char kickMsg[256] = "\0";
-	char name[255] = "\0";
-	char playerKick[255] = "\0";
-	char tempBuffer[255] = "\0";
+	char kickMsg[128] = "\0";
+	char name[32] = "\0";
+	char playerKick[33] = "\0";
+	char tempBuffer[128] = "\0";
 
 	int i=0;
 	size_t newbuflen;
@@ -280,28 +266,17 @@ void processKeys() {
       } else if (strncmp(keyBuff, "say ", 4) == 0) {
         serverNetSendServerMessageAllPlayers((char *) keyBuff+4);
       } else if (strncmp(keyBuff, "kick ", 5) == 0) {
-
-				sprintf(playerKick, "%.*s", 20, keyBuff+5);
-
+				sprintf(playerKick, "%.*s", 32, keyBuff+5);
 				newbuflen = strlen(playerKick);
 				playerKick[newbuflen - 1] = '\0';
-
 				sprintf(tempBuffer, "name: [%s], len: %d\n\n", playerKick, strlen(playerKick));
-				fprintf(stderr, tempBuffer);
-
 				for(i=0;i<=15;i++){
 					playersGetPlayerName(screenGetPlayers(), i, name);
-
 					sprintf(tempBuffer, "name: [%s], len: %d\n", name, strlen(name));
-					fprintf(stderr, tempBuffer);
-
-
 					if(strcmp(playerKick, name) == 0){
-
 						sprintf(kickMsg, "%s has been server kicked.", playerKick);
 						serverNetSendServerMessageAllPlayers(kickMsg);
 						serverNetPlayerLeave(i, TRUE);
-
 					}
 				}
 	  } else if (strncmp(keyBuff, "\n", 1) != 0 && strncmp(keyBuff, "\0", 1) != 0) {
